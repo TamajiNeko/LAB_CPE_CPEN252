a07572cb60f3976cb1ec0128be6d81dd2d959419
TamajiNeko <classroom3.5.17@gmail.com>
2025-08-05 09:30:43 +0700

    LAB04-8-5-25 OOP Concept

--- app/src/main/java/com/tamji/lab/cpe/neko_note/User.java
+++ app/src/main/java/com/tamji/lab/cpe/neko_note/User.java
@@ -1,8 +1,19 @@
 package com.tamji.lab.cpe.neko_note;
 
 public class User {
-    String userName;
-    String passKey;
+    private String userName;
+    private String password;
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String login(String user2Login, String Pass2Login){
+        if(user2Login.toLowerCase() == "admin" & Pass2Login == "1234"){
+            return "Login Successful";
+        }
+        return null;
+    }
 
     void checkUserName(){}
 }
